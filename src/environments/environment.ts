90ffc10d56a3297ed1e1baec8ad841556143cc61
cakirburak <cakirburak43@hotmail.com>
2024-03-25 17:35:46 +0300

    firebase configurations

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyB02RP1x-UXcsQRSm8F1dSlh0uU5TTjcG8",
+    authDomain: "github-like-card.firebaseapp.com",
+    projectId: "github-like-card",
+    storageBucket: "github-like-card.appspot.com",
+    messagingSenderId: "644182258645",
+    appId: "1:644182258645:web:46c449d41fb8bbd7dfc35c"
+  }
+}
